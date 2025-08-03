539a0362946eaaadfa188b02ebd0b25cc838c17d
Term Solver <138608044+t3rm1n4tor@users.noreply.github.com>
2025-08-03 23:04:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Привет с Railway!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Важно!
+    app.run(host='0.0.0.0', port=port)
