390350aae1c73abdfd0f3b23d81c0ef07b71f023
Term Solver <138608044+t3rm1n4tor@users.noreply.github.com>
2025-08-03 23:10:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return 'Приложение работает 🐍'
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Важно!
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
