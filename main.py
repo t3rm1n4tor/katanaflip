1a730ebede4a2335f6227c5791f2d440ba518d64
Term Solver <138608044+t3rm1n4tor@users.noreply.github.com>
2025-08-03 23:19:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ def home():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    print(f"Starting app on port {port}")  # Лог в консоль
     app.run(host='0.0.0.0', port=port)
